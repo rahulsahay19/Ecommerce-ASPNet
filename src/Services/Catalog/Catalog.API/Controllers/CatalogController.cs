3bb5f785b07a7cdf71c5fbd66d57e3013b02f048
rahsahay <rahsahay@WKWIN1353288>
2021-12-05 00:41:58 +0530

    More changes

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>>GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return products;
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -1,7 +1,10 @@
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
 namespace Catalog.API.Data
 {
     public interface ICatalogContext
     {
-        
+        IMongoCollection<Product> Products { get; }
     }
 }
