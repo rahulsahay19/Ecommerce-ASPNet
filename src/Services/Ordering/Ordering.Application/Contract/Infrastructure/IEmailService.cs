c2a126411907990ac7fa3197122f61490c727746
rahsahay <rahsahay@WKWIN1353288>
2021-12-27 16:16:44 +0530

    Initial set of ordering using clean architecture

---
+++ src/Services/Ordering/Ordering.Application/Contract/Infrastructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Ordering.Application.Models;
+
+namespace Ordering.Application.Contract.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    //This will serve as common fields for domain
+    //This means, every entity will have below props by default in ordering Microservice
+    public abstract class EntityBase
+    {
+        //Protected set is made to use in the derived classes
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
