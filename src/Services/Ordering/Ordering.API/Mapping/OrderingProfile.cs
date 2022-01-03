70e0b256bbbd86092ef50e937ab572661ae99ce4
rahsahay <rahsahay@WKWIN1353288>
2022-01-03 22:22:31 +0530

    Finished Rabbit MQ implementation

---
+++ src/Services/Ordering/Ordering.API/Mapping/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Ordering.Application.Feature.Order.Command.CheckoutOrder;
+
+namespace Ordering.API.Mapping
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
